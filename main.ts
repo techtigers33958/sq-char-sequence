6f5f4f60f85e46a0db83cf822c4e280c5eb7853b
techtigers33958 <53841748+techtigers33958@users.noreply.github.com>
2020-03-07 12:23:05 -0500

    fix bug

--- main.ts
+++ main.ts
@@ -1,18 +1,17 @@
 radio.onReceivedString(function (receivedString) {
+    basic.showIcon(IconNames.SmallDiamond)
     if (receivedString == "START") {
         reset()
-    } else {
-        if (receivedString == character) {
-            if (correctCounter == 4) {
-                basic.showIcon(IconNames.Yes)
-                releaseBall()
-            } else {
-                correctCounter += 1
-                generateCharacter()
-            }
+    } else if (receivedString == character) {
+        if (correctCounter == 4) {
+            basic.showIcon(IconNames.Yes)
+            releaseBall()
         } else {
-            basic.showIcon(IconNames.No)
+            correctCounter += 1
+            generateCharacter()
         }
+    } else {
+        basic.showIcon(IconNames.No)
     }
 })
 function releaseBall () {
@@ -22,17 +21,24 @@ function releaseBall () {
 }
 function generateCharacter () {
     character = String.fromCharCode(Math.randomRange(65, 68))
-    basic.showString("" + (character))
+    basic.showString(character)
 }
+input.onButtonPressed(Button.B, function () {
+    reset()
+})
 function reset () {
+    basic.showIcon(IconNames.Diamond)
     correctCounter = 0
     list = []
     generateCharacter()
 }
+input.onButtonPressed(Button.A, function () {
+    releaseBall()
+})
 let list: number[] = []
 let correctCounter = 0
 let character = ""
 radio.setGroup(20)
 pins.analogSetPeriod(AnalogPin.P0, 20000)
 pins.servoSetPulse(AnalogPin.P0, 2000)
-basic.showIcon(IconNames.Heart)
+reset()
